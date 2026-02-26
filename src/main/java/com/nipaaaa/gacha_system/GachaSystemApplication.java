7d39d209f68a3e618b23df1eb66e279010c769b2
Nipaaaa1 <frostone224@gmail.com>
2026-02-26 01:37:59 +0000

    chore: init project

---
+++ src/main/java/com/nipaaaa/gacha_system/GachaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nipaaaa.gacha_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GachaSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GachaSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nipaaaa/gacha_system/GachaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nipaaaa.gacha_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachaSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
